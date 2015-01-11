2552fcd0a2aef79b5caefa282f413f6232f8c3fa
Stephane <geoext@hotmail.com>
2015-01-11 21:41:14 +0100

    Fix code legend plot4.R

--- plot4.R
+++ plot4.R
@@ -34,7 +34,7 @@ plot(df$datetime,df$Sub_metering_1,type="n", xlab = "",ylab="Energy sub metering
 points(df$datetime,df$Sub_metering_1,type="l")
 points(df$datetime,df$Sub_metering_2,type="l",col="red")
 points(df$datetime,df$Sub_metering_3,type="l",col="blue")
-legend("topright",pch="_",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright",lty=c(1,1),col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 #lower right corner
 plot(df$datetime,df$Global_reactive_power,type="l",xlab = "datetime",ylab="Global_reactive_power")
 
